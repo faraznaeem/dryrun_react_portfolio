cef04226e862219c2da8dd05f70aa86734836c73
faraznaeem <faraznaeem@gmail.com>
2020-02-16 22:12:37 +0100

    Adds navigation to header

---
+++ src/Projects.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div className="ui main container">
+      <h1 id="projects-header" className="ui header">
+        My Projects
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem tempore
+        deserunt, magni ratione tempora cum. Eligendi quas, error dolore dolores
+        odio aperiam aspernatur ex eum assumenda! Non saepe numquam at?
+      </p>
+    </div>
+  );
+};
+
+export default Projects;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Hello from "./Hello";
-import Header from './Header'
-import Footer from './Footer'
+import Header from "./Header";
+import Footer from "./Footer";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Projects from "./Projects";
+import About from "./About";
 
-const HelloWorld = () => {
+const App = () => {
   return (
     <div>
-      <Header/>
-      <Hello />
-      <Footer/>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Hello} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/projects" component={Projects} />
+      </Switch>
+      <Footer />
     </div>
-  )
+  );
 };
 
-ReactDOM.render(<HelloWorld />, document.getElementById("app"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("app")
+);
