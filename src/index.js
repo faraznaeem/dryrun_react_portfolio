98dd759082954edb819869da71630796e8d7e3eb
faraznaeem <faraznaeem@gmail.com>
2020-02-16 19:41:08 +0100

    Adds hello component and cypress

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
+import Hello from "./Hello";
 
-const HelloWorld = ()=> {
-  return <h2>Hello World</h2>
+const HelloWorld = () => {
+  return <Hello />;
 };
 
-ReactDOM.render(<HelloWorld/>, document.getElementById("app"))
+ReactDOM.render(<HelloWorld />, document.getElementById("app"));
